bdbde90da4ce95970c9fa6b7352b2a9e4ac6195a
David Majholt <d.boy.dragoz@gmail.com>
2026-05-13 13:36:18 +0200

    docs: Tilføjet kontakt form test med form validering

---
+++ src/tests/ContactForm.test.ts
@@ -0,0 +1,78 @@
+import Contact from "@/components/pages/Contact.vue"
+import { describe, it, expect } from "vitest"
+import { mount } from "@vue/test-utils"
+
+describe("Testområde for kontaktformular", () => {
+
+	it("viser valideringsfejl for ugyldig e-mail", async () => {
+		const wrapper = mount(Contact)
+
+		await wrapper.find("#mail")
+			.setValue("wrong-email")
+
+		await wrapper.find("form")
+			.trigger("submit")
+
+		expect(wrapper.text())
+			.toContain("Du har ikke skrevet en gyldig e-mail.")
+	})
+
+	it("viser valideringsfejl for ugyldigt telefonnummer", async () => {
+		const wrapper = mount(Contact)
+
+		await wrapper.find("#telefon")
+			.setValue("wrong-phone")
+
+		await wrapper.find("form")
+			.trigger("submit")
+
+		expect(wrapper.text())
+			.toContain("Indtasted telefonnummer er ikke gyldigt.")
+	})
+
+	it("viser valideringsfejl for tomt navnefelt", async () => {
+		const wrapper = mount(Contact)
+
+		await wrapper.find("#navn")
+			.setValue("")
+
+		await wrapper.find("form")
+			.trigger("submit")
+
+		expect(wrapper.text())
+			.toContain("Du skal udfylde dette felt.")
+	})
+
+	it("viser valideringsfejl for tomt beskedfelt", async () => {
+		const wrapper = mount(Contact)
+
+		await wrapper.find("#besked")
+			.setValue("")
+
+		await wrapper.find("form")
+			.trigger("submit")
+
+		expect(wrapper.text())
+			.toContain("Du skal udfylde dette felt.")
+	})
+
+	it("accepterer gyldig formular, når den indsendes", async () => {
+		const wrapper = mount(Contact)
+
+		await wrapper.find("#navn")
+			.setValue("John Doe")
+
+		await wrapper.find("#mail")
+			.setValue("john@test.com")
+
+		await wrapper.find("#besked")
+			.setValue("This is a valid message with enough characters.")
+
+		await wrapper.find("form")
+			.trigger("submit")
+
+		expect(wrapper.text())
+			.toContain("Tak for din besked")
+	})
+
+})
